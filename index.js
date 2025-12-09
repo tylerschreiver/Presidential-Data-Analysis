f7bb0a11914b41f9e54a6c3ca4c4fb4378b16735
tylerschreiver <tyschreiver@gmail.com>
2025-12-08 19:18:47 -0500

    Just a small little thought I had and wanted an answer for

---
+++ index.js
@@ -0,0 +1,173 @@
+const fs = require('fs');
+const path = require('path');
+
+// Read and parse both CSV files
+async function readCSVFiles() {
+  try {
+    const presWinnersPath = path.join(__dirname, 'pres_winners.csv');
+    const presWinnersContent = fs.readFileSync(presWinnersPath, 'utf-8');
+
+    const yearAndParty = presWinnersContent.split('\n').map(line => line.split(',')).map(array => [array[0], array[2]])
+
+    const usPresByStatePath = path.join(__dirname, 'us_pres_by_state.csv');
+    const usPresByStateContent = fs.readFileSync(usPresByStatePath, 'utf-8');
+
+    const convertedStateContent = usPresByStateContent.split("\n").map(row => row.split(",")).map(rowData => ({
+      state: rowData[0],
+      votingRecord: rowData.slice(1, rowData.length - 1)
+    }))
+
+    const stateData = convertedStateContent.slice(1, convertedStateContent.length - 1)
+
+    // Extract years from yearAndParty
+    const years = yearAndParty.map(entry => entry[0])
+
+    // Map party abbreviations to full names
+    // Based on pres winners.csv and historical election data
+    // This is AI code, I didn't feel like doing this conversion myself, so I'm just hoping this works
+    const partyMap = {
+      'R': 'Rep.',
+      'D': 'Dem.',
+      'DR': 'D-R',           // Democratic-Republican
+      'Jackson': 'Dem.',      // Andrew Jackson (Democratic)
+      'SD': 'Dem.',           // Southern Democrat (1860)
+      'SR': 'Dem.',           // States' Rights Democratic Party
+      'I': 'Ind.',            // Independent
+      'AI': 'Dem.',           // American Independent Party (treated as Dem. variant)
+      'BM': 'Prog.',          // Bull Moose/Progressive Party (1912)
+      'PO': 'Prog.',          // Progressive Party
+      'GW': 'Ind.',           // George Washington (Independent)
+      'F': 'Fed.',            // Federalist
+      'Adams': 'NR',          // John Quincy Adams (National Republican)
+      'NR': 'NR',             // National Republican
+      'W': 'Whig',            // Whig Party
+      'Crawford': 'D-R',      // William H. Crawford (Democratic-Republican)
+      'LR': 'Rep.',           // Liberal Republican (treated as Rep. variant)
+      'Clay': 'NR',           // Henry Clay (National Republican)
+      'CU': 'Rep.',           // Constitutional Union (1860, treated as Rep. variant)
+      'SP': 'D-R',            // Split/States' Rights (1800, treated as D-R)
+      'KN': 'Dem.',           // Know Nothing/American Party (treated as Dem. variant)
+      'ND': 'Dem.',           // Northern Democrat (1860)
+      'N': 'Dem.',            // Northern (Democrat variant)
+      'AM': 'Whig',           // Anti-Masonic Party (treated as Whig variant)
+      'PR': 'Prog.',          // Progressive Republican (1924)
+      '': '' // Keep empty strings as is
+    }
+
+    // Convert stateData to match yearAndParty format
+    const convertedStateData = stateData.map(state => {
+      const convertedRecord = state.votingRecord.map((party, index) => {
+        const year = years[index]
+        const fullPartyName = partyMap[party] !== undefined ? partyMap[party] : party
+
+        return [year, fullPartyName]
+      })
+      return {
+        state: state.state,
+        votingRecord: convertedRecord
+      }
+    })
+
+    
+    const stateDataWins = convertedStateData.map(stateData => ({
+      state: stateData.state,
+      votingRecord: stateData.votingRecord,
+      votingRecordWins: stateData.votingRecord.map((res, index) => res[1].length ? res[1] === yearAndParty[index][1] : null)
+    }))
+    
+
+    const stateLossStreaks = stateDataWins.map(stateData => {
+      let maxLosses = 0
+      let currentLosses = 0
+
+      let endIndex;
+
+      stateData.votingRecordWins.forEach((res, index) => {
+        if (res === true) {
+          currentLosses = 0
+        } else if (res === false) {
+          currentLosses++
+          if (currentLosses > maxLosses) {
+            maxLosses = currentLosses
+            endIndex = index
+          }
+        } else if (res === null) {
+          // do nothing if empty
+        }
+      })
+
+      return { ...stateData, lossLength: maxLosses, endLossIndex: endIndex }
+    }).sort((a, b) => b.lossLength - a.lossLength)
+
+    let maxLosses = 0
+    stateLossStreaks.forEach(stateData => {
+      if (stateData.lossLength > maxLosses) {
+        maxLosses = stateData.lossLength
+      }
+    })
+
+    const maxLs = stateLossStreaks.filter(data => data.lossLength === maxLosses)
+    
+    maxLs.forEach(stateData => {
+      let votingRecord = ''
+      let winners = ''
+      for (let i = 5; i > 0; i--) {
+        // console.log(years[stateData.endLossIndex - i])
+        // console.log(stateData.votingRecord[stateData.endLossIndex - i][1])
+        const index = stateData.endLossIndex - i + 1
+
+        votingRecord += `${years[index]} - ${stateData.votingRecord[index][1]}  `
+        winners += `${years[index]} - ${yearAndParty[index][1]}  `
+      }
+
+
+      let length = stateData.state.length - 7
+      let isReverse = false
+
+      if (length < 0) {
+        isReverse = true
+        length = length * -1
+      }
+
+      let spaces = ''
+      for (let i = 0; i < length; i++) {
+        spaces += ' '
+      }
+      console.log('\n')
+
+      if (isReverse) {
+        console.log(stateData.state + spaces + " " + votingRecord)
+        console.log('Winners ' + winners)
+      } else {
+        console.log(stateData.state + " " + votingRecord)
+        console.log('Winners ' + spaces + winners)
+      }
+
+    })
+
+    return {
+      stateLossStreaks,
+      maxLs
+    };
+    
+  } catch (error) {
+    console.error('Error reading CSV files:', error);
+    throw error;
+  }
+}
+
+// Execute if run directly
+if (require.main === module) {
+  readCSVFiles()
+    .then(data => {
+      console.log('\n');
+    })
+    .catch(error => {
+      console.error('Failed to read CSV files:', error);
+      process.exit(1);
+    });
+}
+
+// Export the function and data for use in other modules
+module.exports = { readCSVFiles };
+
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Presidential Election Results State by State
+
+This code determines when states voted for the winner or loser of the presidential election
+I wanted to find out which state had the record for the longest streak of not voting for the winner
+
+Assuming this code all works correctly, the answer is 5 straight elections for 6 states:
+- Connecticut (1800-1816)
+- Delaware (1800-1816)
+- Maine (1932-1948)
+- Vermont (1932-1948)
+- Alabama (1952-1968)
+- Mississippi (1952-1968)
+
+CSVs were made by copy pasting tables from Wikipedia into google sheets:
+https://en.wikipedia.org/wiki/List_of_United_States_presidential_elections_by_popular_vote_margin
+https://en.wikipedia.org/wiki/List_of_United_States_presidential_election_results_by_state
+
